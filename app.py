08041162f680f0615923112e5df11bed790a27c1
grace-zan <143233846+grace-zan@users.noreply.github.com>
2023-11-04 12:00:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -26,17 +26,19 @@ def convert():
         action= request.form["threshhold"]
         if action == 'increase':  
             threshold_value = threshold_value + 10
+            print(threshold_value)
             if threshold_value > 255:                    
                 threshold_value = 255
         elif action == 'decrease':
             threshold_value = threshold_value - 10
+            print(threshold_value)
             if threshold_value < 0:
                 threshold_value = 0
         elif action == 'rb':
             rainbow_mode = not rainbow_mode
 
     gen_frames(threshold_value, rainbow_mode)
-    return render_template('convert.html')
+    return render_template('convert.html', threshold=threshold_value)
 
 @app.route('/video_feed')
 def video_feed():
@@ -51,24 +53,46 @@ def upload():
             file_path = os.path.join(app.root_path, 'static/result/og.jpg')
             photo.save(file_path)
 
-            detection_option = request.form.get('Detection Option')
+            detection_option1 = request.form.get('Detection Option1')
 
-            if detection_option == 'Contour':
-                edges_path = contour_edges(file_path)
-            elif detection_option == 'Sobel':
-                edges_path = sobel_edges(file_path)
-            elif detection_option == 'Canny':
-                edges_path = canny_edges(file_path)
+            if detection_option1 == 'Contour':
+                edges_path1 = contour_edges(file_path)
+            elif detection_option1 == 'Sobel':
+                edges_path1 = sobel_edges(file_path)
+            elif detection_option1 == 'Canny':
+                edges_path1 = canny_edges(file_path)
 
-            return render_template('results.html', original=filename, edges=os.path.basename(edges_path))
+            detection_option2 = request.form.get('Detection Option2')
+
+            if detection_option2 == 'Contour':
+                edges_path2 = contour_edges(file_path)
+            elif detection_option2 == 'Sobel':
+                edges_path2 = sobel_edges(file_path)
+            elif detection_option2 == 'Canny':
+                edges_path2 = canny_edges(file_path)
+
+            img1 = cv2.imread(edges_path1)
+            img2 = cv2.imread(edges_path2)
+            
+            dst = cv2.addWeighted(img1, 1, img2, 1, 0)
+
+            edges_path = os.path.join(app.root_path, 'static/result/blended.jpg')
+            # cv2.imwrite(edges_path, dst)
+            
+            # edges_path = os.path.join(app.root_path, 'static/result/inverted.jpg')
+            # img = cv2.imread(edges_path)
+            cv2.imwrite(edges_path, dst)
+
+            return render_template('results.html', ed1 = detection_option1, ed2 = detection_option2)
         return 'No photo uploaded.'
 
 def canny_edges(image_path):
     image = cv2.imread(image_path, 0)
     img_blur = cv2.GaussianBlur(image, (5,5), 0)
-    edges = cv2.Canny(img_blur, 140, 180)
-    edges_path = os.path.join(app.root_path, 'static/result/ed.jpg')
-    cv2.imwrite(edges_path, edges)
+    edges = cv2.Canny(img_blur, 140, 170)
+    img_not = cv2.bitwise_not(edges)
+    edges_path = os.path.join(app.root_path, 'static/result/Canny.jpg')
+    cv2.imwrite(edges_path, img_not)
     return edges_path
 
 def sobel_edges(image_path):
@@ -77,8 +101,9 @@ def sobel_edges(image_path):
     edges = cv2.Sobel(img_blur, cv2.CV_32F, 1, 1, ksize=5)
     # sobely = cv2.Sobel(img_blur, cv2.CV_32F, 0, 1, ksize=5)
     # edges = np.sqrt(sobelx**2 + sobely**2).astype(np.uint8)
-    edges_path = os.path.join(app.root_path, 'static/result/ed.jpg')
-    cv2.imwrite(edges_path, edges)
+    img_not = cv2.bitwise_not(edges)
+    edges_path = os.path.join(app.root_path, 'static/result/Sobel.jpg')
+    cv2.imwrite(edges_path, img_not)
     return edges_path
 
 def contour_edges(image_path):
@@ -90,8 +115,9 @@ def contour_edges(image_path):
     # image_color = (0, 0, 0)
     # image_outlines_only[:] = image_color
     edges = cv2.drawContours(image_outlines_only, contours, -1, (255, 255, 255), 1)
-    edges_path = os.path.join(app.root_path, 'static/result/ed.jpg')
-    cv2.imwrite(edges_path, edges)
+    img_not = cv2.bitwise_not(edges)
+    edges_path = os.path.join(app.root_path, 'static/result/Contour.jpg')
+    cv2.imwrite(edges_path, img_not)
     return edges_path
 
 def gen_frames(threshold_value, rainbow_mode):  
